ec7ad61f334353b4afd77638e86b9ae8d8935e8b
vhgibbs-ux <v.h.gibbs@googlemail.com>
2026-07-22 21:16:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const hour = now.getHours();
 let greeting = "";
 
 if(hour < 12){
-    greeting = "☀️ Good morgon!";
+    greeting = "☀️ God morgon!";
 }
 else if(hour < 18){
     greeting = "🌤️ God afton!";
