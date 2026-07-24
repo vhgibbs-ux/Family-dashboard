f5f8789d2d9797b420daad082bcd8fdcc33084e9
vhgibbs-ux <v.h.gibbs@googlemail.com>
2026-07-24 10:27:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function updateClock(){
+    // Current date and time
 
     const now = new Date();
+
+    // Update today's date
 
     document.getElementById("today").innerHTML =
     now.toLocaleDateString("en-GB",{
@@ -17,6 +20,8 @@ function updateClock(){
     
 const hour = now.getHours();
 
+// Choose a greeting
+    
 let greeting = "";
 
 if(hour < 12){
@@ -29,13 +34,16 @@ else{
     greeting = "🌙 God kväll!";
 }
 
+// Update the bin reminder
 document.getElementById("bin").innerHTML =
 "⬛ Black Bin<br><small>Collection Monday</small>";
-    
+
+// Display the greeting
 document.getElementById("message").innerHTML = greeting;
 
 }
 
 updateClock();
 
+// Refresh every second
 setInterval(updateClock,1000);
